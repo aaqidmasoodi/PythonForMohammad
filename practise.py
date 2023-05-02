7ed9effe4bae99ef5ac4fce3a9e4ccbf66694f6f
aaqidmasoodi <aaqidmasoodi@icloud.com>
2023-05-02 17:08:36 +0530

    session 2 prob 2

--- practise.py
+++ practise.py
@@ -1,4 +1,23 @@
+'''
+    Q:1
+'''
 # create an empty list
 # create the months dictionary upto Jun
-
 # then copy the keys of that dictiory into the list
+
+
+'''
+    Q:2
+'''
+# create an empty list
+# create this dictionary
+'''
+    fares = {
+        'low': [23.65, 3451],
+        'medium': [66.32, 9841],
+        'high': [652.32, 3215],
+    }
+'''
+# add tuples to the empty list which contains the fare type and the sum of the list that corresponds to that fare
+
+# [('low', 3474.65), (), ()]
